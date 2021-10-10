010a306ff91345e6d962ab33c055bea282467eab
Farzan Saketi <farzan.6118@gmail.com>
2021-10-10 13:16:42 +0330

    some changes

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,29 +1,56 @@
 package com;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import com.model.Employee;
+
+import java.io.*;
 
 public class Main {
 
+    public static String pathFile = "D:\\Test\\output.txt";
+    public static BufferedReader br;
+    public static BufferedWriter bw;
+
     public static void main(String[] args) {
+        boolean letsRead = true;
+        var employee = new Employee();
+        employee.setAge(40);
+        employee.setName("devil");
+        employee.setPhoneNumber("09516526563");
+        employee.setSalary("18000000");
 
-        var Employee = new Employee();
-        Employee.setAge(20);
-        Employee.setName("ali");
-        Employee.setPhoneNumber("0916247612");
-        Employee.setSalary("12000000");
-        BufferedWriter bw;
-        var strBuilder = new StringBuilder();
-
+        var main = new Main();
         try {
-            bw = new BufferedWriter(new FileWriter("D:\\Test\\output.txt"));
-            strBuilder.append("start").append("\n\n").append(Employee.toStringTwo()).append("\n").append(Employee).append("\n\n").append("end");
-            bw.write(strBuilder.toString());
-            bw.close();
+            if (letsRead) {
+                System.out.println(main.readFromFile());
+                br.close();
+            } else {
+                main.writeToFile(employee);
+                bw.close();
+            }
         } catch (IOException ioe) {
             System.out.println(ioe);
         }
+    }
+
+    public String readFromFile() throws IOException {
+
+        var strBuilder = new StringBuilder();
+        br = new BufferedReader(new FileReader(pathFile));
+        String s;
+        while ((s = br.readLine()) != null) {
+            strBuilder.append(s);
+        }
+
+        return strBuilder.toString();
+    }
+
+    public void writeToFile(Employee employee) throws IOException {
+
+        var strBuilder = new StringBuilder();
+        bw = new BufferedWriter(new FileWriter(pathFile));
+        strBuilder.append(employee.toStringTwo()).append("\n").append(employee);
+        bw.write(strBuilder.toString());
+
     }
 
 }
--- src/com/Test.java
+++
@@ -1,4 +0,0 @@
-package com;
-
-public record Test() {
-}
